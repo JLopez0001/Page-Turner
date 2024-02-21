73c7295bb937dd416b161a0b24ae023fd28a78bb
John Lopez <john.lopez3695@gmail.com>
2024-02-20 23:10:48 -0500

    Refacored results page to have books be in a row format

--- src/components/search-results/Results.jsx
+++ src/components/search-results/Results.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Book from "../book/Book";
+import "../../styles/book-styles/book.css";
 
 function Results({ results, searchBy }) {
   if (!results.data) {
@@ -11,24 +12,26 @@ function Results({ results, searchBy }) {
 
   return (
     <div>
-      <h1>Your Search Results</h1>
-      {booksToDisplay.map((book, index) => {
-        let title = book.title;
-        // Check if title length is greater than 30 and slice it if necessary
-        if (title.length > 27) {
-          title = title.slice(0, 27) + "..."; // Add ellipsis to indicate truncation
-        }
-        return (
-          <Book
-            key={index}
-            coverImg={book.cover_i}
-            title={title}
-            author={book.author_name[0]}
-            editions={book.edition_count}
-            firstPublished={book.first_publish_year}
-          />
-        );
-      })}
+      <h1 className="results">Your Search Results</h1>
+      <div className="results-container">
+        {booksToDisplay.map((book, index) => {
+          let title = book.title;
+          // Check if title length is greater than 30 and slice it if necessary
+          if (title.length > 27) {
+            title = title.slice(0, 27) + "..."; // Add ellipsis to indicate truncation
+          }
+          return (
+            <Book
+              key={index}
+              coverImg={book.cover_i}
+              title={title}
+              author={book.author_name[0]}
+              editions={book.edition_count}
+              firstPublished={book.first_publish_year}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
