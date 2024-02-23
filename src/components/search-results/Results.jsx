ac768c95e88edbb9ef0f46800b9cbc006c0f8b3e
John Lopez <john.lopez3695@gmail.com>
2024-02-23 01:08:49 -0500

    Added functionality to all more info buttons to take to book details

--- src/components/search-results/Results.jsx
+++ src/components/search-results/Results.jsx
@@ -28,16 +28,13 @@ function Results({ results, searchBy }) {
       <h1 className="results">Your Search Results</h1>
       <div className="results-container">
         {booksToDisplay.map((book, index) => {
-          let title = book.title;
-          // Check if title length is greater than 30 and slice it if necessary
-          if (title.length > 27) {
-            title = title.slice(0, 27) + "..."; // Add ellipsis to indicate truncation
-          }
+          console.log(book);
           return (
             <Book
               key={index}
+              keyId={book.key}
               coverImg={book.cover_i}
-              title={title}
+              title={book.title}
               author={book.author_name[0]}
               editions={book.edition_count}
               firstPublished={book.first_publish_year}
